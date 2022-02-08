7072e49458294d630f3a12e8b9058f100ad83ef8
Dev <asimm364@gmail.com>
2022-02-08 17:41:42 +0500

    Custom routes for devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+  # Build in routes for devise
+  # devise_for :users
+  
+  # Custom routes for devise
+  devise_for :users, path:'', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
